f2944f94e748ac6d9dea765193cd56e6e5acab0f
SINGHxTUSHAR <tusharsinghrawat.delhi@gmail.com>
2023-11-16 11:36:11 +0530

    updated

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,11 @@
 import sys
 from src.logger import logging
 
+# import sys
+# sys.path.insert(0, '../src')
+# import unittest
+# from file1 import *
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
